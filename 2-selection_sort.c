f204b42c28e024516dfd6bc04ebdc9205f0b6d98
tieje <toj320@gmail.com>
2021-06-13 18:43:40 -0400

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * selection_sort - performs a selection sort on array
+ * @array: array data
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int smallest;
+	size_t index_switch = 0;
+	size_t i;
+	size_t j;
+	int tmp;
+
+	for (i = 0; i + 1 < size; i++)
+	{
+		smallest = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < smallest)
+			{
+				smallest = array[j];
+				index_switch = j;
+			}
+		}
+		if (index_switch > i)
+		{
+			tmp = array[i];
+			array[i] = smallest;
+			array[index_switch] = tmp;
+			print_array(array, size);
+		}
+	}
+}
