7a6fc5ec871551e9f22ed6c312cf38d162efe3d7
Carson Stearn <cstearn@gmail.com>
2021-06-14 14:36:28 +0000

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,36 @@
-test push
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of ints in ascending order
+ * using the insertion sort algorithm
+ * @list: pointer to the head node of the doubly linked list
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *swap, *next;
+
+	if (!list || !*list)
+		return;
+
+	swap = (*list)->next;
+	while (swap)
+	{
+		next = swap->next;
+		while (swap->prev && swap->prev->n > swap->n)
+		{
+			swap->prev->next = swap->next;
+			if (swap->next)
+				swap->next->prev = swap->prev;
+			swap->next = swap->prev;
+			swap->prev = swap->next->prev;
+			swap->next->prev = swap;
+			if (swap->prev == NULL)
+				*list = swap;
+			else
+				swap->prev->next = swap;
+			print_list(*list);
+		}
+		swap = next;
+	}
+}
